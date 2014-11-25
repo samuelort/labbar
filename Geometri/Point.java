016b3dc2eb29b71613b94dcfe2c2968f2cb20548
samuelort <ort.samuel@gmail.com>
2014-11-25 14:22:10 +0100

    Update Point.java

--- Geometri/Point.java
+++ Geometri/Point.java
@@ -4,11 +4,24 @@ import java.awt.Graphics;
 import java.awt.*;
 
 public class Point extends AbstractGeometricalForm{
-
+	 /**
+	 * Creates a point.
+	 * @param x horizontal pos.
+	 * @param y vertical pos.
+	 * @param c The fill color.
+	 * @throws IllegalPositionException if any x/y coordinate is negative. 
+	 * 
+	 */
 	public Point(int x, int y, Color c) throws IllegalPositionException{
 		super(x,y,c);
 	}
-	
+	 /**
+	 * Creates a point.
+	 * @param f, takes coordinates from existing point.
+	 * @param c, The fill color.
+	 * @throws IllegalPositionException if any x/y coordinate is negative. 
+	 * 
+	 */
 	public Point(GeometricalForm f, Color c){
 		super(f,c);
 	}
